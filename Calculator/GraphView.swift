0aae9f183eee9e7ef8ceb9561fc4fea6c7402007
Alexander Guschin <alexander.ragon@gmail.com>
2015-09-04 20:32:47 +0300

    Refactoring

--- Calculator/GraphView.swift
+++ Calculator/GraphView.swift
@@ -22,13 +22,9 @@ class GraphView: UIView {
     var origin: CGPoint? { didSet { setNeedsDisplay() } }
     
     private var graphAxes = AxesDrawer(color: UIColor.blackColor())
-    
-    private struct Constants {
-        static let moveGraphGestureScale: CGFloat = 4
-    }
-    
     private var xOffset:CGFloat = 0
     
+    // MARK: drawing functions
     override func drawRect(rect: CGRect) {
         origin = origin ?? graphCenter
         graphAxes.contentScaleFactor = contentScaleFactor
@@ -36,6 +32,33 @@ class GraphView: UIView {
         
         let functionPath = bezierPathForFunctionWithPrecise(0.1)
         functionPath.stroke()
+    }
+    
+    private func bezierPathForFunctionWithPrecise(precise: Double) -> UIBezierPath {
+        let path = UIBezierPath()
+        var range = graphCenter.x + xOffset
+        var leftrange = -2 * graphCenter.x + range
+        var x = Double(range) / pointsPerUnit
+        let leftBoundary = Double(leftrange) / pointsPerUnit
+        
+        // передвигаемся в правый край экрана
+        if let y = dataSource?.funcExecute(x) {
+            path.moveToPoint(CGPointMake(bounds.maxX, bounds.midY - CGFloat(y)))
+        }
+        
+        while x > leftBoundary {
+            if let y = dataSource?.funcExecute(x) {
+                if isLegitValue(y) {
+                    path.addLineToPoint(CGPointMake(origin!.x + CGFloat(x * pointsPerUnit), origin!.y - CGFloat(y * pointsPerUnit)))
+                } else {
+                    // поддержка прерываемых функций
+                    path.moveToPoint(CGPointMake(origin!.x + CGFloat(x * pointsPerUnit), origin!.y - CGFloat(y * pointsPerUnit)))
+                }
+            }
+            x -= precise
+        }
+        
+        return path
     }
     
     // MARK: gesture handlers
@@ -69,32 +92,7 @@ class GraphView: UIView {
         }
     }
     
-    private func bezierPathForFunctionWithPrecise(precise: Double) -> UIBezierPath {
-        let path = UIBezierPath()
-        var range = graphCenter.x + xOffset
-        var leftrange = -2 * graphCenter.x + range
-        var x = Double(range) / pointsPerUnit
-        let leftBoundary = Double(leftrange) / pointsPerUnit
-        
-        // передвигаемся в правый край экрана
-        if let y = dataSource?.funcExecute(x) {
-            path.moveToPoint(CGPointMake(bounds.maxX, bounds.midY - CGFloat(y)))
-        }
-        
-        while x > leftBoundary {
-            if let y = dataSource?.funcExecute(x) {
-                if isLegitValue(y) {
-                    path.addLineToPoint(CGPointMake(origin!.x + CGFloat(x * pointsPerUnit), origin!.y - CGFloat(y * pointsPerUnit)))
-                } else { // поддержка прерываемых функций
-                     path.moveToPoint(CGPointMake(origin!.x + CGFloat(x * pointsPerUnit), origin!.y - CGFloat(y * pointsPerUnit)))
-                }
-            }
-            x -= precise
-        }
-        
-        return path
-    }
-    
+    // MARK: helper functions
     private var graphCenter: CGPoint {
         return convertPoint(center, fromView: superview)
     }
